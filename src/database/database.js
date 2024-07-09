7325759758d3fec430a12a14ecb8d29d9b2619e9
CaroLlanos <61801824+CaroLlanos@users.noreply.github.com>
2024-07-08 20:34:40 -0400

    Update database.js con sshl para el deploy

--- src/database/database.js
+++ src/database/database.js
@@ -10,6 +10,12 @@ export const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
         logging: console.log,
+        dialectOptions: {
+            ssl: {
+                requiere: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 );
 
